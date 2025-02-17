a66155482234959ec24f76320ba6148ca5f53873
Simeoncoded <simeonolawore07@gmail.com>
2025-02-17 13:59:20 -0500

    checking error update

---
+++ Haver/Data/HaverMigrations/20250217185640_cuu.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace haver.Data.HaverMigrations
+{
+    /// <inheritdoc />
+    public partial class cuu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_SalesOrders_SalesOrderID",
+                table: "Machines");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_SalesOrders_SalesOrderID",
+                table: "Machines",
+                column: "SalesOrderID",
+                principalTable: "SalesOrders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_SalesOrders_SalesOrderID",
+                table: "Machines");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_SalesOrders_SalesOrderID",
+                table: "Machines",
+                column: "SalesOrderID",
+                principalTable: "SalesOrders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
