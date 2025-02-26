37fcc2ebeb4a1942bfe628f2d21e955617549b1c
Simeoncoded <simeonolawore07@gmail.com>
2025-02-26 14:42:59 -0500

    update on gantt

--- Haver/ViewModels/GanttViewModel.cs
+++ Haver/ViewModels/GanttViewModel.cs
@@ -3,7 +3,9 @@
     public class GanttViewModel
     {
         public int ID { get; set; }
-        public string MachineName { get; set; }
+
+		public string UniqueID { get; set; } // New unique string identifier
+		public string MachineName { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
