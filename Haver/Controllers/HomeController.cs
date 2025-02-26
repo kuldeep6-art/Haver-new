f50fc8ff15908a70d931c95ff21a701a96ebef5b
Magdalene Akomanyi <magdalene.arhin@outlook.com>
2025-02-26 02:06:06 -0500

    improvedMenuSystem

--- Haver/Controllers/HomeController.cs
+++ Haver/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace haver.Controllers
             return View();
         }
 
+        public IActionResult LandingPage()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
