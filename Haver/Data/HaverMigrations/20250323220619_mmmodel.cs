17b290fc388499bf4e449e715ddd871b32e5ed5f
Simeoncoded <simeonolawore07@gmail.com>
2025-03-23 18:21:10 -0400

    update on machine model

---
+++ Haver/Data/HaverMigrations/20250323220619_mmmodel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace haver.Data.HaverMigrations
+{
+    /// <inheritdoc />
+    public partial class mmmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerApproval",
+                table: "GanttDatas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineTypeID",
+                table: "Machines",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MachineModel",
+                table: "Machines",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineModel",
+                table: "Machines");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineTypeID",
+                table: "Machines",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CustomerApproval",
+                table: "GanttDatas",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
