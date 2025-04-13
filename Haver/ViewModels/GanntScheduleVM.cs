83a5e903c4e4bc92bd2b9cb5170f738ae9c80165
kuldeep6-art <kkumar80@ncstudents.niagaracollege.ca>
2025-04-13 17:25:18 -0400

    Add files via upload

--- Haver/ViewModels/GanntScheduleVM.cs
+++ Haver/ViewModels/GanntScheduleVM.cs
@@ -1,19 +1,21 @@
 ﻿namespace haver.ViewModels
 {
-	public class GanttScheduleViewModel
-	{
-		public string OrderNumber { get; set; }
-		public string Engineer { get; set; }
-		public string CustomerName { get; set; }
-		public int Quantity { get; set; }
-		public string MachineModel {  get; set; }
+
 		//public string Size { get; set; }
 		//public string Class { get; set; }
 		//public string SizeDeck { get; set; }
-		public string Media { get; set; }
-		public string SpareParts { get; set; }
-		public DateTime ApprovedDrawingReceived { get; set; }
-		public List<GanttViewModel> GanttData { get; set; } // Assuming GanttViewModel from previous responses
-		public string SpecialNotes { get; set; }
-	}
+		public class GanttScheduleViewModel
+		{
+			public string OrderNumber { get; set; }
+			public string Engineer { get; set; }
+			public string CustomerName { get; set; }
+			public int Quantity { get; set; }
+			public string MachineModel { get; set; }
+			public string Media { get; set; }
+			public string SpareParts { get; set; }
+			public DateTime ApprovedDrawingReceived { get; set; }
+			public List<GanttViewModel> GanttData { get; set; }
+			public string SpecialNotes { get; set; }
+		}
+	
 }
--- Haver/ViewModels/MachineScheduleVM.cs
+++ Haver/ViewModels/MachineScheduleVM.cs
@@ -2,10 +2,10 @@
 {
 	public class MachineScheduleViewModel
 	{
-		public string SalesOrderNumber { get; set; }
+		public string OrderNumber { get; set; }
 		public string SalesOrderDate { get; set; }
 		public string CustomerName { get; set; }
-		public string MachineDescriptions { get; set; }
+		public string MachineModel { get; set; }
 		public string SerialNumbers { get; set; }
 		public string ProductionOrderNumbers { get; set; }
 		public string PackageReleaseDateE { get; set; }
@@ -13,18 +13,17 @@
 		public string VendorNames { get; set; }
 		public string PoNumbers { get; set; }
 		public string PoDueDates { get; set; }
-		public string DeliveryDates { get; set; }
 		public string Media { get; set; }
 		public string SpareParts { get; set; }
 		public string Base { get; set; }
 		public string AirSeal { get; set; }
 		public string CoatingLining { get; set; }
 		public string Disassembly { get; set; }
+		public string Comments { get; set; }
 		public string PreOrder { get; set; }
 		public string Scope { get; set; }
 		public string ActualAssemblyHours { get; set; }
 		public string ReworkHours { get; set; }
 		public string NamePlate { get; set; }
-		public string Comments { get; set; }
 	}
 }
